8351331e9969016d8694bae5a331ab5ea9a6899a
Hye Ri Kang <estherhrkang@gmail.com>
2021-07-18 02:39:26 -0400

    changed navbar.js name to index.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 // import UsersList from './components/UsersList';
 // import User from './components/User';
