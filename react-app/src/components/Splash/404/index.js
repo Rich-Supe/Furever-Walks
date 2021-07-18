252ce01f24204a1a1e91d392543f4b626ac2b09b
Hye Ri Kang <estherhrkang@gmail.com>
2021-07-17 23:15:11 -0400

    style 404 page

--- react-app/src/components/Splash/404/index.js
+++ react-app/src/components/Splash/404/index.js
@@ -17,12 +17,16 @@ function Page404(){
                 <p className="page-404__4">4</p>
             </div>
             <div className="page-404__text">
-                <p>The resource couldn't be found!</p>
-                {/* <button>Return Home</button> */}
                 <div>
-                <div class="wrapper" onClick={() => history.push("/")}>
-                    <a href="#"><span>Take me home!</span></a>
+                    <p>The resource couldn't be found!</p>
                 </div>
+                {/* <div class="wrapper" onClick={() => history.push("/")}>
+                    <a href="/"><span>Take me home!</span></a>
+                </div> */}
+                <div className="wrapper">
+                    <button onClick={() => history.push("/")}>
+                        <span>TAKE ME HOME</span>
+                    </button>
                 </div>
             </div>
             {/* <img src={dog404} alt="dog 404" className="Page-404__image"></img> */}
