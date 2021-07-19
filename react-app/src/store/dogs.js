bf2178acc2089a5571d90e096c98f9c2416733cf
Rich-Supe <rsupe1993@aol.com>
2021-07-19 18:23:48 -0400

    finished doggo store

--- react-app/src/store/dogs.js
+++ react-app/src/store/dogs.js
@@ -1,6 +1,7 @@
 const SET_DOG = 'dogs/SET_DOG';
 const SET_DOGS = 'dogs/SET_DOGS';
 const ADD_DOG = 'dogs/ADD_DOG';
+const UPDATE_DOG = 'dogs/UPDATE_DOG';
 const REMOVE_DOG = 'dogs/REMOVE_DOG';
 
 const setDog = (id) => ({
@@ -11,6 +12,11 @@ const setDog = (id) => ({
 const setDogs = (dogs) => ({
     type: SET_DOG,
     payload: dogs
+});
+
+const updateDog = (dog) => ({
+    type: UPDATE_DOG,
+    payload: dog
 });
 
 const addDog = (dog) => ({
@@ -50,6 +56,20 @@ export const getDogs = (userId) => async (dispatch) => {
     }
 }
 
+export const editDog = (dogId, dog) => async (dispatch) => {
+    const response = await fetch(`/api/dogs/${dogId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dog)
+    })
+    if (response.ok) {
+        const dog = await response.json();
+        dispatch(updateDog(dog));
+    }
+}
+
 export const createDog = (dog) => async (dispatch) => {
     const response = await fetch(`/api/dogs`, {
         method: 'POST',
@@ -74,18 +94,41 @@ export const deleteDog = (dogId) => async (dispatch) => {
     if (response.ok) {
         dispatch(removeDog(dogId));
     }
+}
     
-    
-    const initialState = {};
+const initialState = {};
 
-    export default function reducer(state = initialState, action) {
-            switch (action.type) {
-                    case SET_DOG:
-                        const newState = {...state};
-                        newState[action.payload] = action.payload;
-                        return newState;
-                    case SET_DOGS:
-                        const newState = {...state};
-                        
-                }
-    }
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        case SET_DOG:
+            const newState = {...state};
+            newState[action.payload] = action.payload;
+            return newState;
+        case SET_DOGS:
+            const newState = {...state};
+            action.payload.forEach((dog) => {
+                newState[dog.id] = dog;
+            });
+            return newState;
+        case UPDATE_DOG:
+            const newState = {...state};
+            newState[action.payload.id] = action.payload;
+            return newState;
+        case ADD_DOG:
+            const newState = {...state};
+            newState[action.payload.id] = action.payload;
+            return newState;
+        case REMOVE_DOG:  
+            const newState = {...state};
+            newState.dog = [...state.dog];
+            const index = newState.dog.findIndex((dog) => dog.id === action.payload);
+            newState.dog.splice(index, 1);
+            return newState;      
+    }   
+}              
+
+// newState = { ...state };
+//             newState.fullNote = [...state.fullNote];
+//             const idxToRemove = newState.fullNote.findIndex(oneFullNote => oneFullNote.id === action.noteId);
+//             newState.fullNote.splice(idxToRemove, 1);
+//             return newState;
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import dogs from './dogs'
 
 const rootReducer = combineReducers({
   session,
+  dogs,
 });
 
 
