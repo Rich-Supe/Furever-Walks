702f3cafd82d041de52676bc54dfee95a577e475
Rich-Supe <rsupe1993@aol.com>
2021-07-20 11:25:58 -0400

    fixed login issues

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -1,14 +1,18 @@
 import styles from '../../css-modules/Profile.module.css'
 import UserInfo from './UserInfo';
 import DogCarousel from './DogCarousel';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 
 function Profile() {
+    const {id} = useParams();
+
     return (
         <div className={styles.profile}>
             <h1>Profile Page!</h1>
             <UserInfo />
-            <DogCarousel />
+            <DogCarousel userId={id}/>
         </div>
 
     );
