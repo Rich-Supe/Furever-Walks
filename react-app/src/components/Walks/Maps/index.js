a2fea69f84e1f33c04799e2902b7ee152df2b522
Rich-Supe <rsupe1993@aol.com>
2021-07-21 18:08:34 -0400

    added map

--- react-app/src/components/Walks/Maps/index.js
+++ react-app/src/components/Walks/Maps/index.js
@@ -1,26 +1,52 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { getKey } from '../../../store/maps';
+import { useEffect, useRef, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
 import Maps from './maps';
+// import styles from 
 
 const MapContainer = () => {
-const key = useSelector((state) => state.maps.key);
-const dispatch = useDispatch();
+    const key = process.env.REACT_APP_MAPS_API_KEY;
+    // Use a useRef
+    const ref = useRef();
 
-useEffect(() => {
-    if (!key) {
-    dispatch(getKey());
+    const [ map, setMap ] = useState();
+    const options = {
+        center: { lat: 37.7749, lng: -122.4194 },
+        zoom: 12,
     }
-}, [dispatch, key]);
 
-if (!key) {
-    return null;
-}
+    useEffect(() => {
+        const onLoad = () =>
+
+          setMap(new window.google.maps.Map(ref.current, {
+            center: { lat: 37.7749, lng: -122.4194 },
+            zoom: 12,
+        }));
+
+        if (!window.google) {
+          const script = document.createElement(`script`);
+          script.src =
+            `https://maps.googleapis.com/maps/api/js?key=` +
+            key;
+          document.head.append(script);
+          script.addEventListener(`load`, onLoad);
+          return () => script.removeEventListener(`load`, onLoad);
+        } else onLoad();
+      }, []);
+    
 
 return (
-    <Maps apiKey={key} />
+    <>
+        <h1> MAPS </h1>
+        <div className="Maps__map" id="map1" ref={ref}
+        style={{ height: 500, width: 500 }}>
+
+        </div>
+    </>
 );
 };
 
 export default MapContainer;
+
+
+
+// Track points to make a route. 
