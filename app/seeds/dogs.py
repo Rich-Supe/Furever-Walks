971d1bbd4db245aa53c3313badd359f0debe1632
Hye Ri Kang <estherhrkang@gmail.com>
2021-07-22 12:50:48 -0400

    attempt to seed dog-walk

--- app/seeds/dogs.py
+++ app/seeds/dogs.py
@@ -1,8 +1,13 @@
-from app.models import db, Dog
+from app.models import db, Dog, Walk, User
 
 def seed_dogs():
     marley = Dog(name='Marley', age=2, breed='Labrador', image_url="", dog_total_distance=0, dog_total_duration=0, dog_total_walks=0, user_id=1)
     luna = Dog(name='Luna', age=8, breed='Shiba', image_url="", dog_total_distance=0, dog_total_duration=0, dog_total_walks=0, user_id=1)
+
+    # dogWalk1 = Walk.query.filter(Walk.user_id == Dog.user_id)
+    # marley.walks.append(dogWalk1)
+    # marley.users.walks.append(dogWalk1)
+    # marley.dogwalks.append(dogWalk1)
 
     db.session.add(marley)
     db.session.add(luna)
