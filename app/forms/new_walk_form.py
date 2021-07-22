409c2c5d51a69513e366d8708dfccd1d926a8ccc
victoriatarane <victoriatarane@gmail.com>
2021-07-22 14:34:43 -0700

    walk forms

--- app/forms/new_walk_form.py
+++ app/forms/new_walk_form.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, FloatField, DateTimeField, BooleanField
+from wtforms import StringField, BooleanField
 from wtforms.validators import ValidationError, DataRequired
 from app.models import Dog
 
 
 class NewWalkForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
-    distance = FloatField('distance', validators=[DataRequired()])
-    duration = IntegerField('duration', validators=[DataRequired()])
-    date = DateTimeField('date', validators=[DataRequired()])
-    rating = IntegerField('rating')
-    status = BooleanField('status')
+    distance = StringField('distance', validators=[DataRequired()])
+    duration = StringField('duration', validators=[DataRequired()])
+    # date = StringField('date', validators=[DataRequired()])
+    rating = StringField('rating')
+    finished = BooleanField('finished')
     routeData = StringField('routeData')
-    user_id = IntegerField('user_id', validators=[DataRequired()])
+    user_id = StringField('user_id', validators=[DataRequired()])
--- react-app/src/components/Walks/index.js
+++ react-app/src/components/Walks/index.js
@@ -4,7 +4,7 @@ import styles from '../../css-modules/Walks.module.css'
 import NewWalk from './NewWalk'
 import WalkInfo from './WalkInfo'
 import WalksCarousel from './WalksCarousel'
-import Maps from './Maps';
+// import Maps from './Maps';
 import { getWalksUser, getWalk } from '../../store/walks';
 
 
@@ -22,7 +22,7 @@ function Walks(){
             <NewWalk />
             <WalksCarousel />
             <WalkInfo />
-            <Maps />
+            {/* <Maps /> */}
         </div>
     )
 }
