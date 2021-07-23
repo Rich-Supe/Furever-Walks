491cc2f25d5222528ede777a645c38d03fd6da26
Rich-Supe <rsupe1993@aol.com>
2021-07-23 17:47:04 -0400

    added callback to persist data from child to parent

--- react-app/src/components/Walks/index.js
+++ react-app/src/components/Walks/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from '../../css-modules/Walks.module.css'
 import NewWalk from './NewWalk'
@@ -12,17 +12,31 @@ function Walks(){
     const dispatch = useDispatch();
     const user = useSelector((state)=> state.session.user)
     let userId = user ? user.id : null
+    const [ mapData, setMapData ] = useState(null)
     // console.log("##############", userId)
     useEffect(()=>{
         dispatch(getWalksUser(2))
     },[dispatch])
+
+
+    // Write a callback function to send as a prop to Maps component
+    function mapCallback (data){
+        setMapData(data) 
+    }
+
+    console.log("---------", mapCallback, "---------")
+
+    if (mapData) {
+        console.log("asdfasdfa;lj", mapData)
+    }
+
     return (
         <div className={styles.walksPage}>
             {/* <h1>Walks</h1> */}
-            <div className={styles.walksFormContainer}><NewWalk /></div>
+            <div className={styles.walksFormContainer}><NewWalk mapData={mapData}/></div>
             <div className={styles.walksCarouselContainer}><WalksCarousel /></div>
             <div className={styles.walksInfoContainer}><WalkInfo /></div>
-            <div className={styles.walksMap}><Maps /></div>
+            <div className={styles.walksMap}><Maps handleCallback={mapCallback}/></div>
         </div>
     )
 }
--- react-app/src/store/maps.js
+++ react-app/src/store/maps.js
@@ -1,27 +1,49 @@
-const LOAD_API_KEY = 'maps/LOAD_API_KEY';
+// const LOAD_API_KEY = 'maps/LOAD_API_KEY';
 
-const loadApiKey = (key) => ({
-  type: LOAD_API_KEY,
-  payload: key,
-});
+// const loadApiKey = (key) => ({
+//   type: LOAD_API_KEY,
+//   payload: key,
+// });
 
-export const getKey = () => async (dispatch) => {
-  const res = await fetch('/api/maps/key');
-  console.log("KEYYY from thunk:", res)
-  const data = await res.json();
-  console.log("KEYYY from thunk2:", res)
-  dispatch(loadApiKey(data.googleMapsAPIKey));
-};
+// export const getKey = () => async (dispatch) => {
+//   const res = await fetch('/api/maps/key');
+//   console.log("KEYYY from thunk:", res)
+//   const data = await res.json();
+//   console.log("KEYYY from thunk2:", res)
+//   dispatch(loadApiKey(data.googleMapsAPIKey));
+// };
 
-const initialState = { key: null };
+// const initialState = { key: null };
 
-const mapsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case LOAD_API_KEY:
-      return { key: action.payload };
-    default:
-      return state;
-  }
-};
+// const mapsReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case LOAD_API_KEY:
+//       return { key: action.payload };
+//     default:
+//       return state;
+//   }
+// };
 
-export default mapsReducer
+// const SET_MAP_STATS = 'maps/SET_MAP_STATS';
+
+// const setMapStats = (stats) => ({
+//     type: SET_MAP_STATS,
+//     payload: stats,
+// });
+
+// export const getMapStats = (stats) => async (dispatch) => {
+//     dispatch(setMapStats(stats));
+// }
+
+// export default function reducer(state = {}, action) {
+//     switch (action.type) {
+//         case SET_MAP_STATS:
+//             // return { ...state, ...action.payload };
+//             const newState = { ...state }
+//             newState["stats"] = action.payload;
+//             return newState;
+
+//         default:
+//             return state;
+//     }
+// }
