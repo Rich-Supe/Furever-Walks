160e9bef4d7056226df32bbf08da7dcbdbca38d2
Hye Ri Kang <estherhrkang@gmail.com>
2021-07-24 11:17:56 -0400

    add nonbreaking space

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -7,17 +7,25 @@ function Footer() {
     return (
         <h3 className={styles.footer}>
             Created by 
-                Esther Kang 
+                Esther Kang
+                    &nbsp;
                     <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/estherhrkang/")}/>
+                    &nbsp;
                     <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/estherhrkang")}/>,
-                Jimson Ma 
+                Jimson Ma
+                    &nbsp;
                     <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/jimson-ma-462197213/")}/>
+                    &nbsp;
                     <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/jimsonm")}/>,
                 Richard Supe
+                    &nbsp;
                     <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/richard-supe/")}/>
+                    &nbsp;
                     <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/Rich-Supe")}/>,
                 Victoria Tarane 
+                    &nbsp;
                     <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/victoria-tarane-54a86b5b/")}/>
+                    &nbsp;
                     <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/victoriatarane")}/>
         </h3>
     )
