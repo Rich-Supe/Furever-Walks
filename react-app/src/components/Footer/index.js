0b64163e8ff0fffe8f8fed812c32fb8a4caaa0fb
Hye Ri Kang <estherhrkang@gmail.com>
2021-07-24 11:49:41 -0400

    fix demo login modal

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,27 +6,29 @@ function Footer() {
 
     return (
         <h3 className={styles.footer}>
-            Created by 
-                Esther Kang
-                    &nbsp;
-                    <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/estherhrkang/")}/>
-                    &nbsp;
-                    <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/estherhrkang")}/>,
-                Jimson Ma
-                    &nbsp;
-                    <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/jimson-ma-462197213/")}/>
-                    &nbsp;
-                    <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/jimsonm")}/>,
-                Richard Supe
-                    &nbsp;
-                    <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/richard-supe/")}/>
-                    &nbsp;
-                    <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/Rich-Supe")}/>,
-                Victoria Tarane 
-                    &nbsp;
-                    <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/victoria-tarane-54a86b5b/")}/>
-                    &nbsp;
-                    <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/victoriatarane")}/>
+            Esther Kang
+                &nbsp;
+                <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/estherhrkang/")}/>
+                &nbsp;
+                <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/estherhrkang")}/>
+                &nbsp;,
+            Jimson Ma
+                &nbsp;
+                <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/jimson-ma-462197213/")}/>
+                &nbsp;
+                <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/jimsonm")}/>
+                &nbsp;,
+            Richard Supe
+                &nbsp;
+                <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/richard-supe/")}/>
+                &nbsp;
+                <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/Rich-Supe")}/>
+                &nbsp;,
+            Victoria Tarane 
+                &nbsp;
+                <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/victoria-tarane-54a86b5b/")}/>
+                &nbsp;
+                <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/victoriatarane")}/>
         </h3>
     )
 }
