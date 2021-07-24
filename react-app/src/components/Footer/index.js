3361640a82232495fa27dffd59a410158dcc5a1a
Hye Ri Kang <estherhrkang@gmail.com>
2021-07-24 15:38:16 -0400

    more styling

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -5,31 +5,36 @@ import linkedin from '../../assets/img/LI-In-Bug.png'
 function Footer() {
 
     return (
-        <h3 className={styles.footer}>
-            Esther Kang
-                &nbsp;
-                <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/estherhrkang/")}/>
-                &nbsp;
-                <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/estherhrkang")}/>
-                &nbsp;,
-            Jimson Ma
-                &nbsp;
-                <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/jimson-ma-462197213/")}/>
-                &nbsp;
-                <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/jimsonm")}/>
-                &nbsp;,
-            Richard Supe
-                &nbsp;
-                <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/richard-supe/")}/>
-                &nbsp;
-                <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/Rich-Supe")}/>
-                &nbsp;,
-            Victoria Tarane 
-                &nbsp;
-                <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/victoria-tarane-54a86b5b/")}/>
-                &nbsp;
-                <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/victoriatarane")}/>
-        </h3>
+        <div className={styles.footer}>
+            <div>
+                <p>Esther Kang</p>
+                    &nbsp;
+                    <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/estherhrkang/")}/>
+                    &nbsp;
+                    <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/estherhrkang")}/>
+            </div>
+            <div>
+                <p>Jimson Ma</p>
+                    &nbsp;
+                    <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/jimson-ma-462197213/")}/>
+                    &nbsp;
+                    <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/jimsonm")}/>
+            </div>
+            <div>
+                <p>Richard Supe</p>
+                    &nbsp;
+                    <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/richard-supe/")}/>
+                    &nbsp;
+                    <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/Rich-Supe")}/>
+            </div>
+            <div>
+                <p>Victoria Tarane</p>
+                    &nbsp;
+                    <img src={linkedin} alt='LinkedIn' onClick={()=> window.open("https://www.linkedin.com/in/victoria-tarane-54a86b5b/")}/>
+                    &nbsp;
+                    <img src={github} alt='GitHub' onClick={()=> window.open("https://github.com/victoriatarane")}/>
+            </div>
+        </div>
     )
 }
 
